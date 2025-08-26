f1875ee0e6d0149d724bc99a089bef51b6859a3e
Triana <triana080704@gmail..com>
2025-08-26 19:53:43 +0700

    update API

--- lib/day23/views/character.dart
+++ lib/day23/views/character.dart
@@ -1,5 +1,6 @@
 // import 'package:flutter/material.dart';
 import 'package:flutter/material.dart';
+import 'package:ppkd_flutter_1/day10/drawer.dart';
 import 'package:ppkd_flutter_1/day23/api/get_character.dart';
 import 'package:ppkd_flutter_1/day23/models/character_model.dart';
 
@@ -14,6 +15,11 @@ class _GetCharacterScreenState extends State<GetCharacterScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(title: Text("Jikan", style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontFamily: "Gilroy_Regular",
+          ),), centerTitle: true, backgroundColor: Colors.blue),
+      drawer: DrawerMenu(),
       body: SingleChildScrollView(
         child: Column(
           children: [
@@ -30,19 +36,22 @@ class _GetCharacterScreenState extends State<GetCharacterScreen> {
                     itemCount: characters.length,
                     itemBuilder: (BuildContext context, int index) {
                       final dataCharacter = characters[index];
-                      return ListTile(
-                        leading: dataCharacter.trailer?.images?.imageUrl == null
-                            ? CircleAvatar()
-                            : Image.network(
-                                dataCharacter.trailer?.images?.imageUrl ?? "",
-                                errorBuilder: (context, error, stackTrace) {
-                                  return CircleAvatar();
-                                },
-                              ),
-                        title: Text(dataCharacter.title ?? ""),
-                        subtitle: Text(
-                          "${dataCharacter.rating} ${dataCharacter.duration}" ??
-                              "",
+                      return Card(
+                        color: const Color.fromARGB(123, 53, 178, 255),
+                        child: ListTile(
+                          leading: dataCharacter.trailer?.images?.imageUrl == null
+                              ? CircleAvatar()
+                              : Image.network(
+                                  dataCharacter.trailer?.images?.imageUrl ?? "",
+                                  errorBuilder: (context, error, stackTrace) {
+                                    return CircleAvatar();
+                                  },
+                                ),
+                          title: Text(dataCharacter.title ?? ""),
+                          subtitle: Text(
+                            "${dataCharacter.rating} - ${dataCharacter.score}" ??
+                                "",
+                          ),
                         ),
                       );
                     },
